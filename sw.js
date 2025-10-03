89047620ee47e6f6500aaa7f839154440e1bebbe
SourabhSuneja <78584922+SourabhSuneja@users.noreply.github.com>
2025-10-03 18:44:24 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Bump the version number when you deploy a new service worker
-const CACHE_NAME = 'ion-spark-v1.0.0.dev.47';
+const CACHE_NAME = 'ion-spark-v1.0.0.dev.48';
 const urlsToCache = [
   '/ion-spark/',
   '/ion-spark/index.html',
